5dead0bc036217abf0936c9939ea58a599a3b871
Ande Caleb <andaeiii@aol.com>
2021-05-15 04:32:40 +0100

    Updated App Structure.... with Material UI compoents and styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core'; //all components starts with caps.. 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import memories from './images/memories.png';       //import image... 
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
+
+import useStyles from './styles';
+
+const App = () => {
+
+    const classes = useStyles();
+
+    return (
+        <Container maxWidth="lg">
+            <AppBar className={classes.appBar} position="static" color="inherit">
+                <Typography className={classes.heading} variant="h2" align="center"> Memories </Typography>
+                <img className={classes.image} src={memories} alt="memories" height="100" />
+            </AppBar>
+
+            <Grow in>
+                <Container>
+                    <Grid container justify="space-between" alignItems="stretch" spacing="3">
+                        <Grid item xs={12} sm={7}>
+                            <Posts />
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Form />
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
+        </Container>
+    )
 }
 
-export default App;
+export default App
---
+++ src/components/Posts/Post/Post.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Post() {
+    return (
+        <div>
+            Single POST
+        </div>
+    )
+}
+
+export default Post;
